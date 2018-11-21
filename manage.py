51df36b9b4110c9350f1c9ae189550cbb4613d7b
Rickyngotho <gicheric14@gmail.com>
2018-11-21 09:51:19 +0300

    function added

--- manage.py
+++ manage.py
@@ -11,7 +11,7 @@ manager = Manager(app)
 migrate = Migrate(app,db)
 manager.add_command('server', Server)
 manager.add_command('db',MigrateCommand)
-
+app.config[" FLASK_ENV"]="development"
 @manager.shell
 def make_shell_context():
     return dict(app = app,db = db,User = User )
